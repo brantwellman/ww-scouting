c492ebdce50b2b455248971eef2ab3b2047a3785
Brant Wellman <brantwellman@gmail.com>
2014-12-03 18:57:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WwScouting::Application.config.secret_key_base = 'c473dbcc91c41a7c6d3846e3ee5d68da137d8e0273c35aa8fe691585cf6e5ccc0ce32849a702fc457415222fa84b4f0afbef7b381cde07b8d6385be6673e3e20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WwScouting::Application.config.session_store :cookie_store, key: '_ww-scouting_session'
